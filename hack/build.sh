b2b91eb67e020d9ca2161613163bbb1dd11a3f18
Ace Eldeib <alexeldeib@gmail.com>
2020-04-21 18:44:50 -0700

    :hammer_and_pick: fix unbound variable

--- hack/build.sh
+++ hack/build.sh
@@ -9,8 +9,8 @@ MDBOOK_VER="v0.3.1"
 # we hardcode linux/amd64 since rust uses a different naming scheme and it's a pain to tran
 echo "downloading mdBook-${MDBOOK_VER}-x86_64-unknown-linux-gnu"
 set -x
-curl -sL -o /tmp/mdbook.${ext} "https://github.com/rust-lang-nursery/mdBook/releases/download/${MDBOOK_VER}/mdBook-${MDBOOK_VER}-x86_64-unknown-linux-gnu.tar.gz"
-tar -C /tmp -xzvf /tmp/mdbook.${ext}
+curl -sL -o /tmp/mdbook.tar.gz "https://github.com/rust-lang-nursery/mdBook/releases/download/${MDBOOK_VER}/mdBook-${MDBOOK_VER}-x86_64-unknown-linux-gnu.tar.gz"
+tar -C /tmp -xzvf /tmp/mdbook.tar.gz
 chmod +x /tmp/mdbook
 
 # Finally build the book.
