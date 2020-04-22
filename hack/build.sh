bb210c35c89b63108fe0f4fb6d2b20ece8639319
Ace Eldeib <alexeldeib@gmail.com>
2020-04-21 18:39:55 -0700

    :hammer_and_wrench: add mdbook build script

---
+++ hack/build.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+set -o errexit
+set -o nounset
+set -o pipefail
+
+MDBOOK_VER="v0.3.1"
+
+# grab mdbook
+# we hardcode linux/amd64 since rust uses a different naming scheme and it's a pain to tran
+echo "downloading mdBook-${MDBOOK_VER}-x86_64-unknown-linux-gnu"
+set -x
+curl -sL -o /tmp/mdbook.${ext} "https://github.com/rust-lang-nursery/mdBook/releases/download/${MDBOOK_VER}/mdBook-${MDBOOK_VER}-x86_64-unknown-linux-gnu.tar.gz"
+tar -C /tmp -xzvf /tmp/mdbook.${ext}
+chmod +x /tmp/mdbook
+
+# Finally build the book.
+/tmp/mdbook build
